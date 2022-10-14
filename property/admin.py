a8bb89a4d1cb658709e9cb2537b8cb48684f2826
dad-siberian <dad.siberian@gmail.com>
2022-10-14 11:15:52 +0300

    linked flats to owners

--- property/admin.py
+++ property/admin.py
@@ -4,11 +4,11 @@ from .models import Flat, Complaint, Owner
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'owner', 'address')
+    search_fields = ('town', 'address')
     readonly_fields = ['created_at']
     list_display = (
-        'address', 'price', 'new_building', 'owners_phonenumber',
-        'owner_pure_phone', 'construction_year', 'town'
+        'address', 'price', 'new_building',
+        'construction_year', 'town'
     )
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
--- property/models.py
+++ property/models.py
@@ -5,11 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(
-        'Нормализированный номер владельца', blank=True
-    )
     new_building = models.BooleanField('Новостройка', null=True)
     liked_by = models.ManyToManyField(
         User,
@@ -82,15 +77,22 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner = models.CharField('ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True
+    )
     owner_pure_phone = PhoneNumberField(
-        'Нормализированный номер владельца', blank=True
+        'Нормализированный номер владельца',
+        blank=True,
+        db_index=True
     )
     owned_flat = models.ManyToManyField(
         Flat,
         related_name="owners",
-        verbose_name='Квартиры в собственности'
+        verbose_name='Квартиры в собственности',
+        db_index=True
     )
 
     def __str__(self):
